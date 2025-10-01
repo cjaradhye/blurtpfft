2f11b502e1633c680d7f4446eb18a8438e9f2700
Zwarup <aradhye.swarup@gmail.com>
2025-10-01 18:56:27 +0530

    fixed stuff

--- client/src/components/Protected.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-function PrivateRoute({ children }) {
-  const token = localStorage.getItem("authToken");
-  return token ? children : <Navigate to="/blurt" replace />;
-}
-
-export default PrivateRoute;
